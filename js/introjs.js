ab672a95a726df2b92b8c1840740310eaa26f498
Arturo-Valdez <74028688+Arturo-Valdez@users.noreply.github.com>
2021-10-02 09:32:24 -0500

    Add files via upload

---
+++ js/introjs.js
@@ -0,0 +1,15 @@
+//alerta en JS
+/*alert('Hola mundo!');
+alert('Hola mundo2!');
+*/
+//CONSOLA EN JS
+/*console.log('hola, saludamos a la consola!');
+console.log(2*2);
+console.info('Mostrando una informacion');
+console.warn('estamos mostrando una advertencia');
+console.error('mostrando un error');*/
+//BORRA TODA INFORMACION DE LA CONSOLA
+console.clear();
+
+//DOCUMENT WRITE
+document.write('<h1>Hola esto es un mensaje en mi web</h1>');
